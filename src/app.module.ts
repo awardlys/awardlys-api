ce648f51baa2ce9f34fba986909126b7356999ad
Jonas <jonasexplore@gmail.com>
2023-12-23 18:09:20 -0300

    feat(database): create database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AwardsModule } from './modules/awards/awards.module';
+import { DatabaseModule } from './modules/database/database.module';
+import { LoggerModule } from './modules/logger/logger.module';
 
 @Module({
-  imports: [],
+  imports: [AwardsModule, DatabaseModule, LoggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
