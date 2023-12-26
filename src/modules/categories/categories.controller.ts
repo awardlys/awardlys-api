c8cb058a3955b62df0a5b5691503f1924a9af660
danielSramos <danieldsrce@gmail.com>
2023-12-26 15:35:24 -0300

    feat: created category module

---
+++ src/modules/categories/categories.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { LoggerService } from '../logger/logger.service';
+import { CategoriesService } from './categories.service';
+import {
+  CreateCategoryInput,
+  UpdateCategoryInput,
+} from './dtos/categories.dto';
+
+@Controller('categories')
+export class CategoriesController {
+  constructor(
+    private readonly logger: LoggerService,
+    private readonly service: CategoriesService,
+  ) {}
+
+  @Get()
+  list() {
+    this.logger.info({}, 'controller > categories > list');
+    return this.service.list();
+  }
+
+  @Post()
+  async create(@Body() body: CreateCategoryInput) {
+    this.logger.info({}, 'controller > categories > create');
+
+    await this.service.create(body);
+
+    this.logger.info({}, 'controller > categories > create > success');
+  }
+
+  @Patch('/:categoryId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async update(
+    @Body() body: UpdateCategoryInput,
+    @Param('categoryId') categoryId: string,
+  ) {
+    this.logger.info({}, 'controller > categories > update');
+
+    await this.service.update(categoryId, body);
+
+    this.logger.info({}, 'controller > categories > update');
+  }
+
+  @Delete(':categoryId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('categoryId') categoryId: string) {
+    await this.service.delete(categoryId);
+  }
+}
